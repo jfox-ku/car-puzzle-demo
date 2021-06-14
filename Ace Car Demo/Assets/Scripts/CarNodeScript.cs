a4823e447a3f82e2c9160aeb7226049101e22800
Atahan Yılmaz <ayilmaz16@ku.edu.tr>
2021-06-14 12:48:43 +0300

    SimpleSetup

---
+++ Ace Car Demo/Assets/Scripts/CarNodeScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarNodeScript : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer SRenderer;
+    public GameColorSO NodeColorSO;
+
+    private void Start() {
+        SRenderer.color = NodeColorSO.GetColor();
+    }
+
+}
---
+++ Ace Car Demo/Assets/Scripts/GameColorSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameColor",menuName ="GameColor/New Color")]
+public class GameColorSO : ScriptableObject
+{
+    public Material baseMaterial;
+
+
+    public Color GetColor() {
+        return baseMaterial.color;
+    }
+}
